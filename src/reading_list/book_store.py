9f90b0e0f20c7ff094175489747ef86cf8bad670
gevanidar <alissongevanidar@proton.me>
2026-05-10 17:40:25 +0200

    Fix failing tests for add favorite book

--- src/reading_list/book_store.py
+++ src/reading_list/book_store.py
@@ -1,13 +1,15 @@
 """Representation of a book store that can contain books."""
 
 from .book import Book
+from .favorite_books import FavoriteBooks
 
 class BookStore:
     """A book store contains a list over available books."""
 
-    def __init__(self, favorite_books=[]):
+    def __init__(self, favorite_books: FavoriteBooks):
         """Initiate an empty bookstore."""
         self.books = []
+        print(f"Creating {favorite_books=} as {type(favorite_books)} and {len(favorite_books.books_id)}")
         self.favorite_books = favorite_books
 
     # addBook, naming convention is snake case for python
@@ -20,7 +22,18 @@ class BookStore:
         """Get a list over the books currently in the book store."""
         return self.books
 
+    def _get_favorite_books(self):
+        """Get a list over the the favorite books object."""
+        return self.favorite_books
+
     # toggleFavorite, naming convention is snake case for python
     def toggle_favorite(self, book_id):
         """Add a book based on the author and title."""
-        # TODO: implement
+
+        print(f"toggle {self.favorite_books=} as {type(self.favorite_books)} and {len(self.favorite_books.books_id)}")
+        if book_id not in self.favorite_books.book_ids:
+            self.favorite_books.add(book_id)
+        else:
+            self.favorite_books.remove(book_id)
+        print(f"toggled {self.favorite_books=} as {type(self.favorite_books)} and {len(self.favorite_books.books_id)}")
+
---
+++ src/reading_list/favorite_books.py
@@ -0,0 +1,21 @@
+"""Representation of the list of my favorite books."""
+
+class FavoriteBooks:
+    """A list of my favorite books."""
+
+    def __init__(self):
+        """Todo."""
+        # TODO: Implement
+        pass
+
+    def add(self, book_id):
+        """Todo."""
+        print("running un implemented method *(add)")
+        # TODO: Implement
+        pass
+
+    def remove(self, book_id):
+        """Todo."""
+        print("running un implemented method *(remove)")
+        # TODO: Implement
+        pass
