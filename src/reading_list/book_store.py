8560f54612a0df5cf6fd88243de638f8fbd52fb9
gevanidar <alissongevanidar@proton.me>
2026-05-11 23:12:35 +0200

    Remove prints

--- src/reading_list/book_store.py
+++ src/reading_list/book_store.py
@@ -9,7 +9,6 @@ class BookStore:
     def __init__(self, favorite_books: FavoriteBooks):
         """Initiate an empty bookstore."""
         self.books = []
-        print(f"Creating {favorite_books=} as {type(favorite_books)} and {len(favorite_books.books_id)}")
         self.favorite_books = favorite_books
 
     # addBook, naming convention is snake case for python
@@ -28,12 +27,9 @@ class BookStore:
 
     # toggleFavorite, naming convention is snake case for python
     def toggle_favorite(self, book_id):
-        """Add a book based on the author and title."""
-
-        print(f"toggle {self.favorite_books=} as {type(self.favorite_books)} and {len(self.favorite_books.books_id)}")
+        """Add a book based on book_id."""
         if book_id not in self.favorite_books.book_ids:
             self.favorite_books.add(book_id)
         else:
             self.favorite_books.remove(book_id)
-        print(f"toggled {self.favorite_books=} as {type(self.favorite_books)} and {len(self.favorite_books.books_id)}")
 
