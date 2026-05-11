c3003d260199cd34ead848a2c0ac9df167635409
gevanidar <alissongevanidar@proton.me>
2026-05-11 23:38:35 +0200

    Fix generation of id

--- src/reading_list/book.py
+++ src/reading_list/book.py
@@ -11,7 +11,7 @@ class Book:
 
     def __generate_book_id(self):
         """Generate a simple id for the book."""
-        return self.author.strip().lower() + "_" + self.title.strip().lower()
+        return self.author.replace(" ", "_").lower() + "-" + self.title.replace(" ", "_").lower()
 
     def get_author(self):
         """Return the author of the book."""
