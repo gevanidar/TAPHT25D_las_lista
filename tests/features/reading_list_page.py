59fc67bc0c6cfc79f84e12fac0f0524a527ab1fc
gevanidar <alissongevanidar@proton.me>
2026-05-24 12:19:41 +0200

    Some more print debugging

--- tests/features/reading_list_page.py
+++ tests/features/reading_list_page.py
@@ -34,7 +34,7 @@ class ReadingListPage:
         locator = self.get_by_test_id(test_id)
         print(f"{locator=}")
         star_selected = locator.get_by_text('star-selected')
-        print(f"{star_selected=}, and {star_selected.selector=}")
+        print(f"{star_selected=}")
         return star_selected 
 
     def get_initial_books(self):
