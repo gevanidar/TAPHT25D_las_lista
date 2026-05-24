566ea15f6e93e3308f829aafb3413f6265f5f734
gevanidar <alissongevanidar@proton.me>
2026-05-24 13:20:02 +0200

    more debugging

--- tests/features/reading_list_page.py
+++ tests/features/reading_list_page.py
@@ -34,6 +34,7 @@ class ReadingListPage:
     def is_favorite_marked(self, test_id):
         row = self.get_by_test_id(test_id)
         div = row.locator('div')
+        print(f'{div=}')
         return div.to_have_class('star-selected')
 
     def get_initial_books(self):
