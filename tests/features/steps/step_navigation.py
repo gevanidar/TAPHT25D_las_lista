ea27513885e650c167151e5d1e2385b1925085da
gevanidar <alissongevanidar@proton.me>
2026-05-24 11:53:07 +0200

    Fix call spelling

--- tests/features/steps/step_navigation.py
+++ tests/features/steps/step_navigation.py
@@ -8,7 +8,7 @@ def step_impl(context):
 
 @when(u'jag trycker på knappen {button}')
 def step_impl(context, button):
-    locator = context.reading_list_page.click_button_by_test_id(button)
+    locator = context.reading_list_page.click_button_with_test_id(button)
 
 @then('bör jag se en lista med {count:d} böcker')
 def step_impl(context, count):
