d6db8cb5379f9183eb244e95fbda371a35a0edab
gevanidar <alissongevanidar@proton.me>
2026-05-24 14:44:52 +0200

    Fixing erros

--- tests/features/steps/step_hover.py
+++ tests/features/steps/step_hover.py
@@ -22,7 +22,7 @@ def step_impl(context):
     first = row.first 
 
     color = first.evaluate("el => window.getComputedStyle(el).backgroundColor")
-    print("{row=}\n{first=}\n{color=}")
+    print(f"{row=}\n{first=}\n{color=}")
 
     assert color != "rgba(0, 0, 0, 0)", "Incorrect selection of element which has hover effect."
     
