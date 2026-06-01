c58e4e473e173f4cc3812aa6867f9daef04b7039
gevanidar <alissongevanidar@proton.me>
2026-06-01 23:39:23 +0200

    Printing

--- src/features/steps/step_book_management.py
+++ src/features/steps/step_book_management.py
@@ -70,13 +70,10 @@ def step_impl(context, title2, author2):
     count = rows.count()
     last_row = rows.nth(count-1)
     last_row_inner = last_row.locator('div')
-    clazz = last_row.get_attribute('class')
-    print(f'{last_row=}\n{clazz=}')
-    clazz = last_row_inner.get_attribute('class')
-    print(f'{last_row_inner=}\n{clazz=}')
 
     row_data_test_id = last_row_inner.get_attribute('data-test-id')
-    row_text = last_row_inner.get_attribute('text')
+    row_book_name = last_row_inner.get_attribute('text')
+    print(f'{row_data_test_id=}\n{row_book_name=}\n')
 
     expected_data_test_id = f'star-{title2}'
     assert expected_data_test_id == row_data_test_id, f"{expected_data_test_id} is not last in the list, {row_data_test_id} is."
