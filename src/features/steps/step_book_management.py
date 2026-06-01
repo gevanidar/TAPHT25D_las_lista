5a4406af5260cc12b2817c6906dc732e2078ed20
gevanidar <alissongevanidar@proton.me>
2026-06-01 23:47:01 +0200

    Use `inner_text()` instead of get_attribute

--- src/features/steps/step_book_management.py
+++ src/features/steps/step_book_management.py
@@ -69,7 +69,7 @@ def step_impl(context, title2, author2):
     last_row_inner = last_row.locator('div')
 
     row_data_test_id = last_row_inner.get_attribute('data-testid')
-    row_book_name = last_row.get_attribute('text')
+    row_book_name = last_row.inner_text()
 
     print(f"{row_data_test_id=}\n{row_book_name=}\n{last_row=}\n{last_row_inner=}")
     expected_data_test_id = f'star-{title2}'
