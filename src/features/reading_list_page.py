aff742927c0e4daa056e39bd9c5bade657722cff
gevanidar <alissongevanidar@proton.me>
2026-06-06 20:08:48 +0200

    Add some print testing

--- src/features/reading_list_page.py
+++ src/features/reading_list_page.py
@@ -63,5 +63,7 @@ class ReadingListPage:
         book_count = int(book_count_text.replace('Listan har ', '').replace(' böcker', ''))
         favorite_count = int(favorite_count_text.replace('Våra användare har hjärtmarkerat ', '').replace(' böcker', ''))
 
+        print(f"{book_count_text=}\n{favorite_count_text=}\n{book_count=}\n{favorite_count=}\n")
+
         return book_count, favorite_count
 
