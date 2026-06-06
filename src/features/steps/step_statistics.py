6e7dc10f22541b7551a7651acfa33f9aab1a5f8e
gevanidar <alissongevanidar@proton.me>
2026-06-06 20:25:55 +0200

    Fix conversion issue

--- src/features/steps/step_statistics.py
+++ src/features/steps/step_statistics.py
@@ -4,14 +4,18 @@ from behave import when, then
 # TODO: Change the strange feature text, since do we really want to test if the text is correct or the count? 
 @then("ska jag se statistik texten för antal böcker Listan har {count} böcker.")
 def step_impl(context, count):
-  books, _ = context.reading_list_page.get_statistics()
+    books, _ = context.reading_list_page.get_statistics()
 
-  assert books == count, f"The number of books should be {count}, but they were {books}."
+    count = int(count)
+
+    assert books == count, f"The number of books should be {count}, but they were {books}."
 
 # TODO: Change the strange feature text, since do we really want to test if the text is correct or the count? 
 @then("ska jag se statistik texten för antal favoritmarkerade böcker Våra användare har hjärtmarkerat {count} böcker.")
 def step_impl(context, count):
-  _, favorites = context.reading_list_page.get_statistics()
+    _, favorites = context.reading_list_page.get_statistics()
 
-  assert favorites == count, f"The number of favorites should be {count}, but they were {favorites}."
+    count = int(count)
+
+    assert favorites == count, f"The number of favorites should be {count}, but they were {favorites}."
 
