87ae013fbffb72c49a7f383311491ccbe0ae6e3e
gevanidar <alissongevanidar@proton.me>
2026-06-06 20:03:16 +0200

    Fix syntax

--- src/features/steps/step_statistics.py
+++ src/features/steps/step_statistics.py
@@ -4,14 +4,14 @@ from behave.api.pending_step import StepNotImplementedError
 
 
 # TODO: Change the strange feature text, since do we really want to test if the text is correct or the count? 
-@then("ska jag se statistik texten för antal böcker Listan har {count} böcker."
+@then("ska jag se statistik texten för antal böcker Listan har {count} böcker.")
 def step_impl(context, count):
   books, _ = context.reading_list_page.get_statistics()
 
   assert books == coun,t f"The number of books should be {count}, but they were {books}."
 
 # TODO: Change the strange feature text, since do we really want to test if the text is correct or the count? 
-@then("ska jag se statistik texten för antal favoritmarkerade böcker Våra användare har hjärtmarkerat {count} böcker. "
+@then("ska jag se statistik texten för antal favoritmarkerade böcker Våra användare har hjärtmarkerat {count} böcker.")
 def step_impl(context, count):
   _, favorites = context.reading_list_page.get_statistics()
 
